5540290006263eae8371125bab0f50e56c0a23d2
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-26 15:36:24 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
+collection = (1..100)
 def my_find(collection)
-
+  i = 0
+  while i < collection.size
+    # binding.pry
+    return collection.to_a[i] if yield(collection.to_a[i])
+    i += 1
+  end
 end
+
+puts my_find(collection) { |x| x % 3 == 0 && x % 5 == 0 }
